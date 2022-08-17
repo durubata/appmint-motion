c66c7e845debd2f88e23c71c35e8d0d91118270d
curlyzik <curlyzik@gmail.com>
2022-08-17 05:19:46 +0100

    auto populate field with existing info

--- src/components/Registration/registration.tsx
+++ src/components/Registration/registration.tsx
@@ -1,11 +1,27 @@
 import { Button, FormControl, FormControlLabel, FormLabel, Grid, Radio, RadioGroup, TextField } from '@mui/material';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { IoIosArrowBack, IoMdSend } from 'react-icons/io';
-import { useViewStore } from 'views-store';
+import { useFormStore, useViewStore } from 'views-store';
 import { SCRegistrationHeader, SCRegistrationBackButton, SCRegistrationForm, SCRegistrationFormBG, ScRegistrationHeaderTitle } from './styles/registrationStyle';
 
 const Registration = () => {
   const { setStateItem } = useViewStore(state => state);
+  const { formItems, setFormItems } = useFormStore(state => state);
+
+  console.log(formItems);
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+
+  useEffect(() => {
+    if (formItems) {
+      setName(formItems.name);
+      setEmail(formItems.email);
+      setPhone(formItems.phone);
+    }
+  }, [formItems]);
+
   return (
     <div>
       <SCRegistrationHeader>
@@ -19,32 +35,40 @@ const Registration = () => {
 
       <SCRegistrationFormBG>
         <SCRegistrationForm>
-          <Grid container spacing={3} justifyContent="center">
-            <Grid item xs={12}>
-              <TextField required id="name" label="Name" fullWidth />
+          <form
+            onSubmit={e => {
+              e.preventDefault();
+              setFormItems({ email, name, phone });
+              setStateItem('Chat');
+            }}
+          >
+            <Grid container spacing={3} justifyContent="center">
+              <Grid item xs={12}>
+                <TextField required id="name" label="Name" fullWidth value={name} onChange={e => setName(e.target.value)} />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField required id="email" label="Email" type="email" fullWidth value={email} onChange={e => setEmail(e.target.value)} />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField required id="phone" label="Phone" type="tel" fullWidth value={phone} onChange={e => setPhone(e.target.value)} />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControl>
+                  <FormLabel id="help">* How can we help you?</FormLabel>
+                  <RadioGroup aria-labelledby="help" defaultValue="female" name="radio-buttons-group">
+                    <FormControlLabel value="female" control={<Radio />} label="Free Chat Widget" />
+                    <FormControlLabel value="male" control={<Radio />} label="Remove 'We're by bata.com'" />
+                    <FormControlLabel value="other" control={<Radio />} label="Other" />
+                  </RadioGroup>
+                </FormControl>
+              </Grid>
+              <Grid item style={{ paddingBottom: '20px' }}>
+                <Button type="submit" variant="contained" style={{ width: '330px', fontWeight: 'bold', backgroundColor: '#03a84e', padding: '10px 0px' }} color="success" startIcon={<IoMdSend />} fullWidth>
+                  Start Chat
+                </Button>
+              </Grid>
             </Grid>
-            <Grid item xs={12}>
-              <TextField required id="email" label="Email" type="email" fullWidth />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField required id="phone" label="Phone" type="tel" fullWidth />
-            </Grid>
-            <Grid item xs={12}>
-              <FormControl>
-                <FormLabel id="help">* How can we help you?</FormLabel>
-                <RadioGroup aria-labelledby="help" defaultValue="female" name="radio-buttons-group">
-                  <FormControlLabel value="female" control={<Radio />} label="Free Chat Widget" />
-                  <FormControlLabel value="male" control={<Radio />} label="Remove 'We're by bata.com'" />
-                  <FormControlLabel value="other" control={<Radio />} label="Other" />
-                </RadioGroup>
-              </FormControl>
-            </Grid>
-            <Grid item style={{ paddingBottom: '20px' }}>
-              <Button variant="contained" style={{ width: '330px', fontWeight: 'bold', backgroundColor: '#03a84e', padding: '10px 0px' }} color="success" startIcon={<IoMdSend />} fullWidth>
-                Start Chat
-              </Button>
-            </Grid>
-          </Grid>
+          </form>
         </SCRegistrationForm>
       </SCRegistrationFormBG>
     </div>
--- src/views-store.ts
+++ src/views-store.ts
@@ -6,7 +6,27 @@ interface ChatStoreProps {
   setStateItem: (screen: keyof typeof HOME_VIEWS) => any;
 }
 
+interface FormItems {
+  name: string;
+  email: string;
+  phone: string;
+}
+
+interface FormStoreProps {
+  formItems: FormItems;
+  setFormItems: (items: { name: string; email: string; phone: string }) => void;
+}
+
 export const useViewStore = create<ChatStoreProps>(set => ({
-  currentView: 'Chat',
+  currentView: 'Welcome',
   setStateItem: (screen: keyof typeof HOME_VIEWS) => set((state: any) => ({ currentView: screen })),
+}));
+
+export const useFormStore = create<FormStoreProps>(set => ({
+  formItems: {
+    name: '',
+    phone: '',
+    email: '',
+  },
+  setFormItems: (items: FormItems) => set(state => ({ formItems: items })),
 }));

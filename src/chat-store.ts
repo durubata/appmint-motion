3f4d030acfe0fb176dc7dd03eaae4b24905e524a
curlyzik <curlyzik@gmail.com>
2022-08-24 19:05:35 +0100

    Created store for file upload

--- src/chat-store.ts
+++ src/chat-store.ts
@@ -1,4 +1,5 @@
-import create from 'zustand'
+import { FILETYPE } from 'components/chat-message';
+import create from 'zustand';
 
 interface ChatStoreProps {
   sidebar?: string;
@@ -8,11 +9,29 @@ interface ChatStoreProps {
   message?: any;
 }
 
-export const useChatStore = create<ChatStoreProps>((set) => ({
+interface FileItemsProps {
+  fileType: FILETYPE;
+  fileUrl: string;
+  file: File;
+}
+interface FileUploadStoreProps {
+  fileItems: FileItemsProps | null;
+  setFileItems: (items: FileItemsProps) => void;
+  removeFileItems: () => void;
+}
+
+export const useChatStore = create<ChatStoreProps>(set => ({
   messages: {},
   contacts: {},
   groups: {},
   message: {},
   sidebar: '',
   setStateItem: (item: { [key: string]: any }) => set((state: any) => ({ ...item })),
+}));
+
+export const useFileUploadStore = create<FileUploadStoreProps>(set => ({
+  fileItems: null,
+  file: null,
+  setFileItems: item => set(state => ({ fileItems: item })),
+  removeFileItems: () => set({ fileItems: null }),
 }));
